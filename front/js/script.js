29fb19cf8ce842f76a88c4e9ee5a40bb5bd6f38b
Jordan Bachelez <bachelezjordan@gmail.com>
2022-06-04 11:14:39 +0200

    Insérer les produits dans la page d’accueil

---
+++ front/js/script.js
@@ -0,0 +1,26 @@
+const dataProduct = document.getElementById("items");
+const errorApi = document.querySelector(".titles");
+
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
+  .then((obj) => {
+    sofa(obj);
+  })
+  .catch((error) => {
+    errorApi.innerHTML = "<h1>404 NOT FOUND</h1>";
+    console.log("Error api:" + " " + error);
+  });
+
+const sofa = (index) => {
+  for (let product of index) {
+    dataProduct.innerHTML += `
+    <a href="./product.html?_id=${product._id}">
+    <article>
+      <img src="${product.imageUrl}" alt="${product.altTxt}">
+      <h3 class="productName">${product.name}</h3>
+      <p class="productDescription">${product.description}</p>
+    </article>
+  </a>
+  `;
+  }
+};
